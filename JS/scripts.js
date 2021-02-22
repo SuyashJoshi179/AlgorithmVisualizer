c0fa776e6c2edd6eeae0e2a1413071ac5d285784
SuyashJoshi179 <suyash.joshi179@gmail.com>
2021-02-22 10:10:30 +0530

    Changed curser to pointer on hover. Used CSS :hover instead of jQuery .hover. jQuery .hover is deprecated.

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,15 +1,4 @@
-let item = document.getElementById("mybox")
 
-console.log(item);
-
-
-
-$(".card").hover(function(){
-    $(this).css("box-shadow","0 15px 20px 0 rgba(0, 0, 0, 0.2), 0 15px 30px 0 rgba(0, 0, 0, 0.19)");
-    $(this).effect("scale");
-    }, function(){
-    $(this).css("box-shadow", "0 0 0");
-  });
 
 
 
